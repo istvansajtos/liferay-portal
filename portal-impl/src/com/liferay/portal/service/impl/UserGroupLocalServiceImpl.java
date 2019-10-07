2e629429773bf56d0272aa73137e8160758a038c
drewbrokke <drew.brokke@liferay.com>
2019-10-07 16:24:21 +0200

    LPS-92932 portal-impl - (UserGroup*ServiceImpl) - fixes NPE and restores original conditional logic

--- portal-impl/src/com/liferay/portal/service/impl/UserGroupLocalServiceImpl.java
+++ portal-impl/src/com/liferay/portal/service/impl/UserGroupLocalServiceImpl.java
@@ -1188,10 +1188,13 @@ public class UserGroupLocalServiceImpl extends UserGroupLocalServiceBaseImpl {
 		Indexer<?> indexer = IndexerRegistryUtil.nullSafeGetIndexer(
 			UserGroup.class);
 
-		if (indexer.isIndexerEnabled() &&
-			PropsValues.USER_GROUPS_SEARCH_WITH_INDEX &&
-			MapUtil.isEmpty(params)) {
+		if (!indexer.isIndexerEnabled() ||
+			!PropsValues.USER_GROUPS_SEARCH_WITH_INDEX) {
 
+			return true;
+		}
+
+		if (MapUtil.isEmpty(params)) {
 			return false;
 		}
 
