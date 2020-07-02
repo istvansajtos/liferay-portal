d6fc96cd3c3f8a5f3072a4b0a2d8571616ddc9f5
Istvan Sajtos <istvan.sajtos@liferay.com>
2020-07-02 11:47:02 +0200

    LPS-88338 Add util method

--- portal-impl/src/com/liferay/portal/util/PortalImpl.java
+++ portal-impl/src/com/liferay/portal/util/PortalImpl.java
@@ -6166,6 +6166,18 @@ public class PortalImpl implements Portal {
 	}
 
 	@Override
+	public boolean isForwarded(HttpServletRequest httpServletRequest) {
+		String requestURI = (String)httpServletRequest.getAttribute(
+			JavaConstants.JAVAX_SERVLET_FORWARD_REQUEST_URI);
+
+		if (requestURI != null) {
+			return true;
+		}
+
+		return false;
+	}
+
+	@Override
 	public boolean isForwardedSecure(HttpServletRequest httpServletRequest) {
 		if (PropsValues.WEB_SERVER_FORWARDED_PROTOCOL_ENABLED) {
 			String forwardedProtocol = httpServletRequest.getHeader(
