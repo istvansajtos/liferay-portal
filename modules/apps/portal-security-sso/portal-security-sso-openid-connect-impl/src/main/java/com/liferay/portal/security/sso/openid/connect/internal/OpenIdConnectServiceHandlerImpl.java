fb0d3b622c71cfaf1c27e5b25cb11623c3f8b091
Istvan Sajtos <istvan.sajtos@liferay.com>
2020-12-16 11:34:53 +0100

    LPS-124963 Non-serializable object in OpenId Connect session breaks session replication

--- modules/apps/portal-security-sso/portal-security-sso-openid-connect-impl/src/main/java/com/liferay/portal/security/sso/openid/connect/internal/OpenIdConnectServiceHandlerImpl.java
+++ modules/apps/portal-security-sso/portal-security-sso-openid-connect-impl/src/main/java/com/liferay/portal/security/sso/openid/connect/internal/OpenIdConnectServiceHandlerImpl.java
@@ -417,7 +417,7 @@ public class OpenIdConnectServiceHandlerImpl
 
 		openIdConnectSessionImpl.setLoginUserId(userId);
 
-		openIdConnectSessionImpl.setUserInfo(userInfo);
+		openIdConnectSessionImpl.setUserInfoJSONObject(userInfo.toJSONObject());
 	}
 
 	protected boolean refreshAuthToken(
