321933ddc1295d8734de1c78114adee9fdb62885
Istvan Sajtos <istvan.sajtos@liferay.com>
2024-03-01 10:33:32 +0100

    LPD-17785 Update action and render commands

--- modules/apps/login/login-web/src/main/java/com/liferay/login/web/internal/portlet/action/LoginMVCActionCommand.java
+++ modules/apps/login/login-web/src/main/java/com/liferay/login/web/internal/portlet/action/LoginMVCActionCommand.java
@@ -57,6 +57,7 @@ import org.osgi.service.component.annotations.Reference;
  */
 @Component(
 	property = {
+		"javax.portlet.name=" + LoginPortletKeys.CREATE_ACCOUNT,
 		"javax.portlet.name=" + LoginPortletKeys.FAST_LOGIN,
 		"javax.portlet.name=" + LoginPortletKeys.LOGIN,
 		"mvc.command.name=/login/login"
