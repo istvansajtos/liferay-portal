9da4b6c74988456937ea346fa86df3782de032e4
Istvan Sajtos <istvan.sajtos@liferay.com>
2024-04-03 09:30:30 +0200

    LPD-6869 If no default Create Account utility page exists, then we should stay in the current portlet context when changing view

--- modules/apps/login/login-web/src/main/java/com/liferay/login/web/internal/servlet/taglib/include/CreateAccountNavigationPostPageInclude.java
+++ modules/apps/login/login-web/src/main/java/com/liferay/login/web/internal/servlet/taglib/include/CreateAccountNavigationPostPageInclude.java
@@ -9,20 +9,30 @@ import com.liferay.layout.utility.page.kernel.constants.LayoutUtilityPageEntryCo
 import com.liferay.layout.utility.page.kernel.provider.LayoutUtilityPageEntryLayoutProvider;
 import com.liferay.login.web.constants.LoginPortletKeys;
 import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.exception.NoSuchLayoutException;
 import com.liferay.portal.kernel.feature.flag.FeatureFlagManager;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.model.Company;
 import com.liferay.portal.kernel.model.Layout;
+import com.liferay.portal.kernel.portlet.PortletURLFactoryUtil;
+import com.liferay.portal.kernel.portlet.url.builder.PortletURLBuilder;
+import com.liferay.portal.kernel.service.LayoutLocalService;
 import com.liferay.portal.kernel.theme.ThemeDisplay;
 import com.liferay.portal.kernel.util.JavaConstants;
 import com.liferay.portal.kernel.util.Portal;
 import com.liferay.portal.kernel.util.Validator;
 import com.liferay.portal.kernel.util.WebKeys;
+import com.liferay.portal.util.PropsValues;
 import com.liferay.taglib.include.PageInclude;
 import com.liferay.taglib.ui.IconTag;
 
 import java.util.Objects;
 
 import javax.portlet.PortletConfig;
+import javax.portlet.PortletMode;
+import javax.portlet.PortletRequest;
+import javax.portlet.WindowState;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.jsp.JspException;
@@ -86,14 +96,21 @@ public class CreateAccountNavigationPostPageInclude implements PageInclude {
 		try {
 			String url = StringPool.BLANK;
 
-			Layout layout =
-				_layoutUtilityPageEntryLayoutProvider.
-					getDefaultLayoutUtilityPageEntryLayout(
-						themeDisplay.getScopeGroupId(),
-						LayoutUtilityPageEntryConstants.TYPE_CREATE_ACCOUNT);
+			if (_featureFlagManager.isEnabled("LPD-6378")) {
+				Layout layout =
+					_layoutUtilityPageEntryLayoutProvider.
+						getDefaultLayoutUtilityPageEntryLayout(
+							themeDisplay.getScopeGroupId(),
+							LayoutUtilityPageEntryConstants.
+								TYPE_CREATE_ACCOUNT);
 
-			if (layout != null) {
-				url = _portal.getLayoutURL(layout, themeDisplay);
+				if (layout != null) {
+					url = _portal.getLayoutURL(layout, themeDisplay);
+				}
+				else {
+					url = _getCreateAccountURL(
+						httpServletRequest, themeDisplay);
+				}
 			}
 			else {
 				url = _portal.getCreateAccountURL(
@@ -109,8 +126,66 @@ public class CreateAccountNavigationPostPageInclude implements PageInclude {
 		iconTag.doTag(pageContext);
 	}
 
+	private String _getCreateAccountURL(
+			HttpServletRequest httpServletRequest, ThemeDisplay themeDisplay)
+		throws Exception {
+
+		if (Validator.isNull(PropsValues.COMPANY_SECURITY_STRANGERS_URL)) {
+			long plid = themeDisplay.getPlid();
+
+			Layout layout = themeDisplay.getLayout();
+
+			if (layout.isPrivateLayout()) {
+				plid = _layoutLocalService.getDefaultPlid(
+					layout.getGroupId(), false);
+			}
+
+			PortletConfig portletConfig =
+				(PortletConfig)httpServletRequest.getAttribute(
+					JavaConstants.JAVAX_PORTLET_CONFIG);
+
+			return PortletURLBuilder.create(
+				PortletURLFactoryUtil.create(
+					httpServletRequest, portletConfig.getPortletName(), plid,
+					PortletRequest.RENDER_PHASE)
+			).setMVCRenderCommandName(
+				"/login/create_account"
+			).setParameter(
+				"saveLastPath", false
+			).setPortletMode(
+				PortletMode.VIEW
+			).setWindowState(
+				WindowState.MAXIMIZED
+			).buildString();
+		}
+
+		try {
+			Layout layout = _layoutLocalService.getFriendlyURLLayout(
+				themeDisplay.getScopeGroupId(), false,
+				PropsValues.COMPANY_SECURITY_STRANGERS_URL);
+
+			return _portal.getLayoutURL(layout, themeDisplay);
+		}
+		catch (NoSuchLayoutException noSuchLayoutException) {
+
+			// LPS-52675
+
+			if (_log.isDebugEnabled()) {
+				_log.debug(noSuchLayoutException);
+			}
+		}
+
+		return StringPool.BLANK;
+	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		CreateAccountNavigationPostPageInclude.class);
+
 	@Reference
 	private FeatureFlagManager _featureFlagManager;
+
+	@Reference
+	private LayoutLocalService _layoutLocalService;
 
 	@Reference
 	private LayoutUtilityPageEntryLayoutProvider
