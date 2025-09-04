d1fb732088ecd863a2dd8bfc8cc8e14c7954fc72
Istvan Sajtos <istvan.sajtos@liferay.com>
2025-09-04 22:01:22 +0200

    add test

---
+++ modules/sdk/gradle-plugins-patcher/src/test/com/liferay/gradle/plugins/patcher/JarPostProcessorPluginTest.java
@@ -0,0 +1,36 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2025 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.gradle.plugins.patcher;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author Istvan Sajtos
+ */
+public class JarPostProcessorPluginTest {
+
+	@Before
+	public void setUp() {
+	}
+
+	@After
+	public void tearDown() {
+	}
+
+	@Test
+	public void testApply() {
+	}
+
+	@Test
+	public void testProcessManifest() {
+	}
+
+	@Test
+	public void testProcessPom() {
+	}
+
+}
